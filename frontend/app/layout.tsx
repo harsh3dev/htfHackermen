15bbd4a640f85b2a90ef137761e2cce7e8ddbc90
Harsh Pandey <pandey73harsh@gmail.com>
2024-11-09 14:04:24 +0530

    add navigation menu

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from 'next/font/google'
+import Navbar from "@/components/Navbar";
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning >
       <body
-        className={`${inter.className} dark antialiased`}
+        className={`${inter.className} bg-gradient-to-b from-black via-violet-950 to-black text-white dark antialiased`}
+        suppressHydrationWarning
       >
+        <Navbar />
         {children}
       </body>
     </html>
